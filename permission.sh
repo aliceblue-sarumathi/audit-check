a830b3b78ee6c48046b49e5a0a993705cc129b00
aliceblue-sarumathi <159526461+aliceblue-sarumathi@users.noreply.github.com>
2024-12-10 13:31:09 +0530

    Update permission.sh

--- permission.sh
+++ permission.sh
@@ -16,5 +16,8 @@ chmod og-rwx /etc/cron.d/
 echo "***After permission changes****"
 ls -ld /etc/cron*
 
+echo "Configured permissions on /etc/sshd_Config "
+chmod u-x,og-rwx /etc/ssh/sshd_config chown root:root /etc/ssh/sshd_config sudo systemctl restart ssh
+
 echo  "*****Banner is configured or not **************"
 sshd -T -C user=root -C host="$(hostname)" -C addr="$(grep $(hostname) /etc/hosts | awk '{print $1}')" | grep banner
